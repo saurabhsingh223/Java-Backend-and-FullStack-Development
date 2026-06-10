1f6ad6a5d18c83125275c8299e8b8bbe32a206fc
saurabhsingh223 <sksinghs318@gmail.com>
2026-06-11 01:24:54 +0530

    Updated method overloading examples

--- Oops/BasicOOPs.java
+++ Oops/BasicOOPs.java
@@ -3,6 +3,22 @@ class Calculator{
         return a+b;
     }
 }
+// Method Overloading ---> same method name, different parameter
+
+class Calculator2{
+    int num; // Instance variable
+    int add(int a, int b){
+        return a+b;
+    }
+
+    int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    double add(double a, double c){
+        return a+c;
+    }
+}
  
 public class BasicOOPs {
     public static void main(String [] args){
